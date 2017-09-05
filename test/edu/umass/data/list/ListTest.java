b0426f68d9f1b2fcbe3fa794a3a1a8b859fe2046
bkushigian <bkushigian@gmail.com>
2017-09-05 16:43:37 +0000

    Created some basic unit tests

---
+++ test/edu/umass/data/list/ListTest.java
@@ -0,0 +1,50 @@
+package edu.umass.data.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListTest {
+    List<Integer> nil = new List<>();
+    List<Integer> l1  = new List<>(new Integer[] {0,1,2,3});
+    List<Integer> l2  = new List<>(new Integer[] {0,1,2,3,4,5,6,7});
+    @Before
+    public void setUp(){
+
+    }
+    @Test
+    public void testCons01(){
+        List<Integer> l = nil.cons(1);
+        assertEquals((Integer)1, (Integer)l.head);
+    }
+    @Test
+    public void testCons02(){
+        List<Integer> l = nil.cons(1).cons(2);
+        assertEquals((Integer)2, l.head);
+        assertEquals((Integer)1, l.tail.head);
+        assertEquals(null, l.tail.tail.head);
+    }
+    @Test
+    public void testCons03(){
+        List<Integer> l = nil.cons(1).cons(2).cons(3);
+        assertEquals((Integer)3, l.head);
+        assertEquals((Integer)2, l.tail.head);
+        assertEquals((Integer)1, l.tail.tail.head);
+        assertEquals(null, l.tail.tail.tail.head);
+    }
+
+    @Test
+    public void testEquals01(){
+        List<Integer> l = nil.cons(3).cons(2).cons(1).cons(0);
+        assertEquals(l, l1);
+        assertEquals(l1, l);
+    }
+
+    @Test
+    public void testEquals02(){
+        List<Integer> l = nil.cons(3).cons(2).cons(1).cons(0).cons(0);
+        assertNotEquals(l, l1);
+        assertNotEquals(l1, l);
+    }
+}

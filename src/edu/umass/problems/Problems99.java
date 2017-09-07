48c89b1681e012044c8c9d8545cd890ca994bc77
bkushigian <bkushigian@gmail.com>
2017-09-07 11:56:17 +0000

    Created Problems99

---
+++ src/edu/umass/problems/Problems99.java
@@ -0,0 +1,15 @@
+package edu.umass.problems;
+import edu.umass.data.List;
+import edu.umass.data.Maybe;
+import edu.umass.data.Maybe.*;
+
+/**
+ * This is a rough take on the 99 Haskell Problems (and 99 Prolog Problems
+ * (and 99 Lisp Problems)).
+ */
+
+public class Problems99 {
+    public <T> Maybe<T> findLast(List<T> ts){
+        return null;
+    }
+}

bc7f577abb67cc2bf320083b323cb5b3f031b20c
bkushigian <bkushigian@gmail.com>
2017-09-07 09:03:24 +0000

    Imported static methods from ListUtil and renamed all ListUtil.foo to foo

--- test/edu/umass/data/list/ListUtilTest.java
+++ test/edu/umass/data/list/ListUtilTest.java
@@ -3,6 +3,7 @@ package edu.umass.data.list;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static edu.umass.data.list.ListUtil.*;
 
 public class ListUtilTest {
     List<Integer> nil = new List<>();
@@ -13,48 +14,55 @@ public class ListUtilTest {
     public void testIntersperes(){
         List<Integer> expected = new List<Integer>(new Integer[] {0,1,0,1,0});
         List<Integer> xs       = new List<Integer>(new Integer[] {0,0,0});
-        List<Integer> actual   = ListUtil.intersperse(1, xs);
+        List<Integer> actual   = intersperse(1, xs);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testConcat(){
-        List<Integer> expected = l2;
-        List<Integer> left     = new List<>(new Integer[] {0,1,2,3});
-        List<Integer> right    = new List<>(new Integer[] {4,5,6,7});
-        List<Integer> actual   = ListUtil.concat(left, right);
-        assertEquals(left, right);
+        List<Integer> expected = range(0,8);
+        List<Integer> left     = range(0,4);
+        List<Integer> right    = range(4,8);
+        List<Integer> actual   = concat(left, right);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void testFlatten(){
-        List<Integer> l1 = ListUtil.range(0,3);
-        List<Integer> l2 = ListUtil.range(3,6);
-        List<Integer> l3 = ListUtil.range(6,9);
+        List<Integer> l1 = range(0,3);
+        List<Integer> l2 = range(3,6);
+        List<Integer> l3 = range(6,9);
         List<List<Integer>> toFlatten = new List<List<Integer>>()
                 .cons(l3)
                 .cons(l2)
                 .cons(l1);
-        List<Integer> expected = ListUtil.range(9);
-        List<Integer> actual = ListUtil.flatten(toFlatten);
+        List<Integer> expected = range(9);
+        List<Integer> actual = flatten(toFlatten);
         assertEquals(expected,actual);
     }
 
     @Test
     public void testMap(){
         List<Integer> expected = new List<>(new Integer[] {1,2,3,4});
-        List<Integer> actual   = ListUtil.map((x -> x + 1), l1);
+        List<Integer> actual   = map((x -> x + 1), l1);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testSize(){
-        assertEquals(4, ListUtil.size(l1));
+        assertEquals(4, size(l1));
     }
 
     @Test
     public void testNil(){
         assertTrue(nil.nil());
+    }
+
+    @Test
+    public void testFoldr(){
+        Integer actual = foldr((x,y) -> {return x * y;}, 1, range(1,10));
+        Integer expected = 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2;
+        assertEquals(expected,actual);
     }
 
 }

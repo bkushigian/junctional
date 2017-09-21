8af9100b94e4ac2b314b126f04d992418343db24
bkushigian <bkushigian@gmail.com>
2017-09-21 22:14:15 +0000

    Factored out Nothing and Just into separate files

--- src/edu/umass/data/Maybe.java
+++ src/edu/umass/data/Maybe.java
@@ -1,25 +1,6 @@
 package edu.umass.data;
 
 public abstract class Maybe<T> {
-
-    public final boolean nothing;
-
-    protected Maybe(boolean nothing){
-        this.nothing = nothing;
-    }
-    public class Just<T> extends Maybe<T>{
-        final public T val;
-        public Just(T val){
-            super(false);
-            if (val == null) throw new NullPointerException();
-            this.val = val;
-        }
-    }
-
-    public class Nothing<T> extends Maybe<T>{
-        public Nothing(){
-            super(true);
-        }
-    }
-
+    public abstract boolean isNothing();
+    public abstract boolean isJust();
 }
